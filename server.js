f79b3474dc12c0874018bd558d6220d8f5d969e8
sotrnguy92 <sonnguyen092@gmail.com>
2020-12-08 18:27:39 -0800

    got routes to work, was missing PORT on the listener

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const express = require('express');
-
-
-const PORT = 3000;
+const logger = require("morgan");
+const PORT = 3001;
 
 const app = express();
 
+app.use(logger("dev"));
+
 app.use(express.urlencoded({extended:true}));
-app.use(express.json);
+app.use(express.json());
 
 app.use(express.static("public"));
 
-app.listen(() => {
+app.use(require("./routes/view.js"));
+app.use(require("./routes/api.js"));
+
+
+app.listen(PORT,() => {
     console.log(`app is running on port ${PORT} `);
 });
